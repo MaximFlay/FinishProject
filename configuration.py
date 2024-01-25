80ed3844501688419918ede5bf3ebe64646257e7
MaximFlay <maximflay1990@gmail.com>
2024-01-25 15:28:50 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://e15d9e66-b52a-498c-8149-a0e55fa0b819.serverhub.praktikum-services.ru"
+ORDER_PATH = "/api/v1/orders"
+GET_ORDER_PATH = "/api/v1/orders/track"
---
+++ sender_stand_request.py
@@ -0,0 +1,14 @@
+import configuration
+import data
+import requests
+#Создание нового заказа
+def post_new_order(body_order):
+    return requests.post(configuration.BASE_URL + configuration.ORDER_PATH,
+                         json=body_order,
+                         headers=data.headers)
+
+#Получение заказа по треку
+def get_order(track):
+    return requests.get(configuration.BASE_URL + configuration.GET_ORDER_PATH,
+                         params={"t":track},
+                         headers=data.headers)
